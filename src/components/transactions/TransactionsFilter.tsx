bbf96929bb10dd0537dd8018985894696a89dc87
Roozbeh-Lazemzadeh <rz.lazem@gmail.com>
2024-04-03 09:34:22 +0330

    attempt fixing slider #6

--- src/components/transactions/TransactionsFilter.tsx
+++ src/components/transactions/TransactionsFilter.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import FilterTools from '../template/FilterTools';
-import './style.css';
-import { TransactionsList } from './TransactionsList';
+import React from "react";
+import FilterTools from "../template/FilterTools";
+import "./style.css";
+import { TransactionsList } from "./TransactionsList";
+import Slider from "../slider/Slider";
 
 export const TransactionsFilter: React.FC = () => {
+  const homeSliderArray = [
+    { img: "/assets/banner-sidbar/photo101.png" },
+    { img: "/assets/banner-sidbar/photo101.png" },
+    { img: "/assets/banner-sidbar/photo101.png" },
+    { img: "/assets/banner-sidbar/photo101.png" },
+    // { img: '/assets/banner-sidbar/photo102.png' },
+    // { img: '/assets/banner-sidbar/photo103.png' },
+    // { img: '/assets/banner-sidbar/photo104.png' }
+  ];
   return (
     <div className="transaction-filter">
       <FilterTools title="تراکنش‌های اخیر" />
+      <div
+        style={{
+          borderBottom: "1px solid rgba(5, 5, 5, 0.06)",
+          width: 220,
+          margin: "20px auto",
+        }}
+      >
+        <Slider ImgArray={homeSliderArray} />
+      </div>
       <TransactionsList />
     </div>
   );
