5be4ed45f312ff693bfac229748836f534369ade
Roozbeh-Lazemzadeh <rz.lazem@gmail.com>
2024-04-12 16:56:30 +0330

    fix bugs clicking on the active tab

--- src/components/layout/PrimaryFooter.tsx
+++ src/components/layout/PrimaryFooter.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import { Layout, Tabs } from 'antd';
-import { ReactComponent as DefaultPaymanIcon } from '../../icons/defaultPayman.svg';
-import { ReactComponent as DefaultPaperIcon } from '../../icons/defaultPaper.svg';
-import { ReactComponent as DefaultHomeIcon } from '../../icons/defaultHome.svg';
-import { ReactComponent as ActivePaperIcon } from '../../icons/activePaper.svg';
-import { ReactComponent as ActivePaymanIcon } from '../../icons/activePayman.svg';
-import { ReactComponent as ActiveHomeIcon } from '../../icons/activeHome.svg';
-import './style.css';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../hooks/reduxHooks';
-import { selectFilteredFooter } from '../../store/footer/footerSlice';
+import { useEffect, useState } from "react";
+import { Layout, Tabs } from "antd";
+import { ReactComponent as DefaultPaymanIcon } from "../../icons/defaultPayman.svg";
+import { ReactComponent as DefaultPaperIcon } from "../../icons/defaultPaper.svg";
+import { ReactComponent as DefaultHomeIcon } from "../../icons/defaultHome.svg";
+import { ReactComponent as ActivePaperIcon } from "../../icons/activePaper.svg";
+import { ReactComponent as ActivePaymanIcon } from "../../icons/activePayman.svg";
+import { ReactComponent as ActiveHomeIcon } from "../../icons/activeHome.svg";
+import "./style.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAppSelector } from "../hooks/reduxHooks";
+import { selectFilteredFooter } from "../../store/footer/footerSlice";
 
 const { Footer } = Layout;
 interface TabData {
@@ -24,24 +24,24 @@ const PrimaryFooter: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const isPrimaryFooterShown = !useAppSelector(selectFilteredFooter);
-  const [activeTab, setActiveTab] = useState('2');
+  const [activeTab, setActiveTab] = useState("2");
 
   useEffect(() => {
     // Get the path from the current location
-    const pathSegments = location.pathname.split('/');
+    const pathSegments = location.pathname.split("/");
 
     // Get the first segment (excluding the empty string before the first '/')
     const firstSegment = pathSegments[1];
 
     // Map the path to the corresponding tab key
     const tabKeyMap: Record<string, string> = {
-      transactions: '1',
-      home: '2',
-      paymans: '3',
+      transactions: "1",
+      home: "2",
+      paymans: "3",
     };
 
     // Set the active tab based on the mapped key or default to '2'
-    setActiveTab(tabKeyMap[firstSegment] || '2');
+    setActiveTab(tabKeyMap[firstSegment] || "2");
   }, [location.pathname]); // Re-run the effect when the pathname changes
   const handleTabChange = (key: string): void => {
     setActiveTab(key);
@@ -49,30 +49,31 @@ const PrimaryFooter: React.FC = () => {
 
   const tabData: TabData[] = [
     {
-      key: '1',
-      label: 'تراکنش ها',
+      key: "1",
+      label: "تراکنش ها",
       defaultIcon: <DefaultPaperIcon />,
       activeIcon: <ActivePaperIcon />,
-      path: 'transactions',
+      path: "transactions",
     },
     {
-      key: '2',
-      label: 'خانه',
+      key: "2",
+      label: "خانه",
       defaultIcon: <DefaultHomeIcon />,
       activeIcon: <ActiveHomeIcon />,
-      path: 'home',
+      path: "home",
     },
     {
-      key: '3',
-      label: 'پیمان ها',
+      key: "3",
+      label: "پیمان ها",
       defaultIcon: <DefaultPaymanIcon />,
       activeIcon: <ActivePaymanIcon />,
-      path: 'paymans',
+      path: "paymans",
     },
   ];
 
-  const handleTabClick = (key: string): void => {
+  const handleTabClick = (key: string): null | void => {
     // find label by key
+    if (key === activeTab) return null;
     const tab = tabData.filter((tab) => tab.key === key)[0];
     const { path } = tab;
     navigate(path);
@@ -80,7 +81,7 @@ const PrimaryFooter: React.FC = () => {
   return (
     <Footer
       className={`${
-        isPrimaryFooterShown ? 'primary-footer-active' : 'primary-footer'
+        isPrimaryFooterShown ? "primary-footer-active" : "primary-footer"
       }`}
     >
       <Tabs
