8e251b63c96975a2c6c56494408c363b5e852039
mehdirn <derismehdi@yahoo.com>
2024-04-13 11:30:25 +0330

    befor push

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -11,7 +11,7 @@ import { ReactComponent as BlogIcon } from "../../icons/defaultBlog.svg";
 import "./style.css";
 // import Slider from "../slider/Slider";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, EffectCards, Pagination, EffectFlip } from "swiper/modules";
+import { Autoplay, EffectCards, Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/effect-cards";
@@ -209,7 +209,7 @@ export const Sidebar: React.FC = () => {
         <Slider ImgArray={homeSliderArray} />
       </div> */}
       <Swiper
-        effect={"flip"}
+        effect={"cards"}
         slidesOffsetAfter={3}
         spaceBetween={30}
         centeredSlides={true}
@@ -220,7 +220,7 @@ export const Sidebar: React.FC = () => {
         pagination={{
           clickable: true,
         }}
-        modules={[Autoplay, Pagination, EffectCards, EffectFlip]}
+        modules={[Autoplay, Pagination, EffectCards]}
         className="slider-home"
       >
         {/* <SwiperSlide className="banner-home">
