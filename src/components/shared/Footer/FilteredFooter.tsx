e5edbf445743a1ed776b591dd2f3c8e1bcd845ff
Roozbeh-Lazemzadeh <rz.lazem@gmail.com>
2024-04-17 11:11:44 +0330

    WIP filtering merchants

--- src/components/shared/Footer/FilteredFooter.tsx
+++ src/components/shared/Footer/FilteredFooter.tsx
@@ -2,49 +2,67 @@ import { Layout, Menu, Badge } from 'antd';
 import { ReactComponent as ActivityIcon } from '../../../icons/activity.svg';
 import { ReactComponent as WorkIcon } from '../../../icons/work.svg';
 import { ReactComponent as CalendarIcon } from '../../../icons/calendar.svg';
-import { ReactComponent as TickSquareIcon } from '../../../icons/tickSquare.svg';
+import { ReactComponent as RemoveIcon } from '../../../icons/delete.svg';
 import { getItem } from '../../helpers';
-import './style.css';
-import { useAppSelector } from '../../hooks/reduxHooks';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
 import {
   selectFilteredFooter,
   selectSearchedFooter,
 } from '../../../store/footer/footerSlice';
 import { selectAllFilter } from '../../../store/filter/filterSlice';
+import './style.css';
+import { useNavigate } from 'react-router-dom';
 
 const { Footer } = Layout;
 
 const FilteredFooter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const isFilteredFooterShown = useAppSelector(selectFilteredFooter);
   const isSearchedFooterShown = useAppSelector(selectSearchedFooter);
-  const filteredNumber = useAppSelector(selectAllFilter);
-  // const dispatch = useAppDispatch();
+  const allFilter = useAppSelector(selectAllFilter);
+  console.log(allFilter);
+  const removeFilterItem =
+    allFilter?.merchants?.length !== 0
+      ? getItem({
+          key: '101',
+          label: 'حذف فیلترها',
+          icon: <RemoveIcon />,
+          badge: 0,
+          className: 'remove-filter-item',
+          dispatch,
+          navigate,
+        })
+      : null;
+
   const items = [
-    getItem({
-      key: '101',
-      label: 'اعمال',
-      icon: <TickSquareIcon />,
-      badge: 0, // Initialize badge count
-    }),
+    // Conditionally include the item based on a state
+    removeFilterItem!,
     getItem({
       key: '102',
       label: 'مبلغ',
       icon: <ActivityIcon />,
-      badge: 0, // Initialize badge count
+      badge: 0,
+      dispatch,
+      navigate,
     }),
     getItem({
       key: '103',
       label: 'تاریخ',
       icon: <CalendarIcon />,
-      badge: 0, // Initialize badge count
+      badge: 0,
+      dispatch,
+      navigate,
     }),
     getItem({
       key: '104',
       label: 'کسب و کار',
       icon: <WorkIcon />,
-      badge: filteredNumber.merchants.length || 0, // Initialize badge count
+      badge: allFilter.merchants.length || 0,
+      dispatch,
+      navigate,
     }),
-  ];
+  ].filter(Boolean);
 
   return (
     <Footer
@@ -59,6 +77,7 @@ const FilteredFooter: React.FC = () => {
         selectedKeys={[location.pathname]}
         items={items.map((item) => ({
           ...item,
+          className: item.className,
           icon:
             item.badge !== 0 ? (
               <Badge count={item.badge}>{item.icon}</Badge>
